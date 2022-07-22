39aa74d08b15f3913042d715f1776f8f27570959
cbb <470557347@qq.com>
2022-07-22 13:43:08 +0800

    use eureka

--- springcloud-provider-dept-8001/src/main/java/com/chen/springcloud/DeptProvider_8001.java
+++ springcloud-provider-dept-8001/src/main/java/com/chen/springcloud/DeptProvider_8001.java
@@ -2,11 +2,12 @@ package com.chen.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 //import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 //启动类
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 public class DeptProvider_8001 {
     public static void main(String[] args) {
         SpringApplication.run(DeptProvider_8001.class,args);
