02121a42ee49d7c6dfb16c0ed72fea22053386f5
cbb <470557347@qq.com>
2022-07-22 19:33:14 +0800

    change the load balancing strategy

--- springcloud-consumer-dept-8002/src/main/java/com/chen/springcloud/DeptConsumer_9000.java
+++ springcloud-consumer-dept-8002/src/main/java/com/chen/springcloud/DeptConsumer_9000.java
@@ -1,11 +1,14 @@
 package com.chen.springcloud;
 
+import com.chen.myrule.MyRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name = "SPRINGCLOUD-PROVIDER-DEPT", configuration = MyRule.class)
 public class DeptConsumer_9000 {
     public static void main(String[] args) {
         SpringApplication.run(DeptConsumer_9000.class, args);
